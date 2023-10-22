76bd47e10039b9a2709c413be253072a3c98bb6e
a.fadayi <fadarash14@gmail.com>
2023-10-22 11:12:28 +0330

    uncomment .env.local from .gitignore

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,7 @@ export default async function middleware(req: NextRequest) {
   const cookieToken = req.cookies.get("login_token");
   const login_token_env = process.env.TOKEN;
   const isTokenValid = login_token_env === cookieToken?.value;
-  console.log(cookieToken);
   const pathName = req.nextUrl.pathname;
-  console.log(isTokenValid);
   if (!isTokenValid && pathName.startsWith("/controlling")) {
     return NextResponse.redirect(new URL("/", req.url));
   }
